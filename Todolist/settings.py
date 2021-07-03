403aba6482c681ea4d65776d1138bb3dba3c2fe0
Prakhar Sharma <prakharentp2002@gmail.com>
2021-07-03 20:43:11 +0530

    Update settings.py

--- Todolist/settings.py
+++ Todolist/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'q5(_6+(5)0o-@0-2@614q@%9jcb-vo#a!#=1y0o6$w4ffksk3!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["tododailylist.herokuapp.com"]
 
